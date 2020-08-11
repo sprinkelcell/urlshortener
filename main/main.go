d3db7dc420a9c8e1cc48fda7f6d6ad31a6a24f6e
kalPesh <sprinkelcell@gmail.com>
2020-08-11 22:10:11 +0530

    update

--- main/main.go
+++ main/main.go
@@ -19,13 +19,14 @@ func main() {
 
 	// Build the YAMLHandler using the mapHandler as the
 	// fallback
-
+	/// Build the YAMLHandler using the mapHandler as the
+	// fallback
 	yaml := `
-		- path: /urlshort
-		  url: https://github.com/gophercises/urlshort
-		- path: /urlshort-final
-		  url: https://github.com/gophercises/urlshort/tree/solution
-		`
+- path: /urlshort
+  url: https://github.com/gophercises/urlshort
+- path: /urlshort-final
+  url: https://github.com/gophercises/urlshort/tree/solution
+`
 	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
 	if err != nil {
 		panic(err)
